b04593af31f2024a96db6d9b7cf416a14599654a
nicolelara123 <nicole.lara.luz@escola.pr.gov.br>
2026-08-20 11:19:30 -0300

    funçoes

---
+++ java.js
@@ -0,0 +1,14 @@
+function iniciar jogo() {
+    sorteio um númrero entre 1 e 100
+    numeroSecreto=
+    math.floor(Math.random() *100)+ 1;
+    tentativas= 0;
+    reseta a tela
+    document. getElementByld ('entradaNumero').
+    value= '';
+    document.getElementByld ('mensagem').textO
+    ontent= '';
+    document.getElementByld ('botaoReniciar')
+    .classList.add('escondido');
+    
+}
